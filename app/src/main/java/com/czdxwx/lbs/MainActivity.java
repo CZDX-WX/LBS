4ecfa248e841fa18e07008bfc9ef5a74e57429d7
CZDXWX1226566881 <1226566881@qq.com>
2021-04-11 17:51:30 +0800

    第一版

--- app/src/main/java/com/czdxwx/lbs/MainActivity.java
+++ app/src/main/java/com/czdxwx/lbs/MainActivity.java
@@ -1,75 +1,165 @@
 package com.czdxwx.lbs;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.graphics.Bitmap;
+import android.app.ActionBar;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-import com.amap.api.maps.AMap;
-import com.amap.api.maps.LocationSource;
-import com.amap.api.maps.MapView;
-import com.amap.api.maps.model.BitmapDescriptor;
-import com.amap.api.maps.model.MyLocationStyle;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends Activity {
-    AMap aMap = null;
-    MapView mMapView = null;
+import com.heinrichreimersoftware.materialdrawer.DrawerView;
+import com.heinrichreimersoftware.materialdrawer.structure.DrawerHeaderItem;
+import com.heinrichreimersoftware.materialdrawer.structure.DrawerItem;
+import com.heinrichreimersoftware.materialdrawer.structure.DrawerProfile;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerView drawer;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //获取地图控件引用
-        mMapView = (MapView) findViewById(R.id.map);
-        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
-        mMapView.onCreate(savedInstanceState);// 此方法须覆写，虚拟机需要在很多情况下保存地图绘制的当前状态。
-        //初始化地图控制器对象
-        if (aMap == null) {
-            aMap = mMapView.getMap();
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        drawer = findViewById(R.id.drawer);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            public void onDrawerClosed(View view) {
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                invalidateOptionsMenu();
+            }
+        };
+
+        drawerLayout.setStatusBarBackgroundColor(ContextCompat.getColor(this, R.color.color_primary_dark));
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerLayout.closeDrawer(drawer);
+
+        //第一个item:活动推荐
+        drawer.addItem(new DrawerItem().setImage(ContextCompat.getDrawable(this, R.drawable.notice))
+                .setTextPrimary(getString(R.string.first_item))
+                .setTextSecondary(getString(R.string.first_information)));
+
+        //第二个item:空教室查询
+        drawer.addItem(new DrawerItem().setRoundedImage((BitmapDrawable) ContextCompat.getDrawable(this, R.drawable.search))
+                .setTextPrimary(getString(R.string.second_item))
+                .setTextSecondary(getString(R.string.second_information))
+        );
+
+
+        //第三个item:校园新闻
+        drawer.addItem(new DrawerItem().setRoundedImage((BitmapDrawable) ContextCompat.getDrawable(this, R.drawable.news))
+                .setTextPrimary(getString(R.string.third_item))
+                .setTextSecondary(getString(R.string.third_information))
+        );
+
+
+        //进入相关页面
+        drawer.setOnItemClickListener(new DrawerItem.OnItemClickListener() {
+            @Override
+            public void onClick(DrawerItem item, long id, int position) {
+                drawer.selectItem(position);
+                switch (position){
+                    case 0:
+                        //跳转到活动推荐页面
+
+                        break;
+                    case 1:
+                        //跳转到空教室查询页面
+
+                        break;
+                    case 2:
+                        //跳转到校园新闻页面
+
+                        break;
+                }
+            }
+        });
+
+        drawer.addFixedItem(new DrawerItem().setRoundedImage((BitmapDrawable) ContextCompat.getDrawable(this, R.drawable.team), DrawerItem.SMALL_AVATAR).setTextPrimary(getString(R.string.Team)));
+
+
+        drawer.setOnFixedItemClickListener(new DrawerItem.OnItemClickListener() {
+            @Override
+            public void onClick(DrawerItem item, long id, int position) {
+                drawer.selectFixedItem(position);
+                Toast.makeText(MainActivity.this,"没写功能", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        //缩略图
+        drawer.addProfile(new DrawerProfile()
+                .setId(1)
+                .setRoundedAvatar((BitmapDrawable) ContextCompat.getDrawable(this, R.drawable.head))
+                .setBackground(ContextCompat.getDrawable(this, R.drawable.bg))
+                .setName(getString(R.string.main))
+                .setDescription(getString(R.string.person))
+        );
+
+        //点击暂无功能
+        drawer.setOnProfileClickListener(new DrawerProfile.OnProfileClickListener() {
+            @Override
+            public void onClick(DrawerProfile profile, long id) {
+                Toast.makeText(MainActivity.this,"暂时没啥功能", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
 
-        aMap.setTrafficEnabled(true);// 显示实时交通状况
-        //地图模式可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
-        aMap.setMapType(AMap.MAP_TYPE_SATELLITE);// 卫星地图模式
-        //定位蓝点
-        MyLocationStyle myLocationStyle;
-        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        myLocationStyle.showMyLocation(true);
-//        myLocationStyle.myLocationIcon(new BitmapDescriptor(Bitmap.createBitmap()));
-        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        //跳转到github主页
+        if (item.getItemId() == R.id.action_github) {
+            String url = "https://github.com/CZDXWX1226566881/LBS";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        }
 
-        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-//        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
-        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
-        mMapView.onDestroy();
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
-        mMapView.onResume();
-    }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
-        mMapView.onPause();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
-        mMapView.onSaveInstanceState(outState);
+    //activity加载完全后的回调函数
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
     }
 }
--- app/src/main/java/com/czdxwx/lbs/MyApplication.java
+++ app/src/main/java/com/czdxwx/lbs/MyApplication.java
@@ -1,12 +1,24 @@
 package com.czdxwx.lbs;
 
+
+import android.annotation.SuppressLint;
 import android.app.Application;
+import android.content.Context;
+
+
 
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        QMUISwipeBackActivityManager.init(this);
+        context=getApplicationContext();
+
+    }
+
+    private static Context context;
+
+    public static Context getContext(){
+        return context;
     }
 }
